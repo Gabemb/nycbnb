6a4744394ff12ab4b23cda23546bc2cebf2575ab
michael <michaeldinnall@gmail.com>
2017-01-05 15:09:36 -0500

    App routes

---
+++ back/routes /index.js
@@ -0,0 +1,10 @@
+/////////////////////////
+// Index page - import and organize all api routes. export for use in server
+/////////////////////////
+const router = require('express').Router();
+
+router.use('/users', require('./userRoutes.js'))
+router.use('/listing', require('./listingsRoutes.js'))
+router.use('/booking', require('./bookingRoutes.js'))
+
+module.exports = router;

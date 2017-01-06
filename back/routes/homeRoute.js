269551261ecb050b5c4da8cedf977cba58a9b89c
Gabemb <gabrielbny@gmail.com>
2017-01-06 11:36:23 -0500

    home router fix

--- back/routes/homeRoute.js
+++ back/routes/homeRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = ('path');
 
 module.exports = app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../../front/views/index.html'))
